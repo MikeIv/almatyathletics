086e9c1f761c3d084b4b5f9bb05058bc2600945c
MikeIv <mikeuivanov@gmail.com>
2017-12-04 09:04:50 +0300

    final reliz

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+var slides = document.querySelectorAll('.intro__slides .intro__slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,5000);
+
+function nextSlide(){
+  slides[currentSlide].className = 'intro__slide';
+  currentSlide = (currentSlide+1)%slides.length;
+  slides[currentSlide].className = 'intro__slide-showing';
+}
